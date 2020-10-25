0e5977f5ebf57055921189feea9c41ac5c728f7b
jabides <jabides@riverlogic.com>
2020-10-24 18:31:20 -0700

    Add gameboy svg elements

--- src/components/gameboy.jsx
+++ src/components/gameboy.jsx
@@ -5,7 +5,7 @@ import GameboyScreen from "./gameboyScreen";
 class Gameboy extends Component {
   state = {
     animationState: 0,
-    animationStateDurations: [0, 1600, 1600, 600, 1000],
+    animationStateDurations: [0, 1600, 1600, 2000, 1000],
   };
 
   render() {
@@ -13,7 +13,21 @@ class Gameboy extends Component {
   }
 
   screenOnlyRender = () => {
-    if (this.state.animationState !== 4) return "";
+    if (this.state.animationState < 3) return (
+      <svg viewBox="0 0 256 128" width="100%" height="100%">
+        <rect className="screen" x="46" y="18" width="164" height="92" fill="black"></rect>
+        <g className="cross-pad">
+          <rect x="12" y="60" width="24" height="8" fill="black"></rect>
+          <rect x="20" y="52" width="8" height="24" fill="black"></rect>
+        </g>
+        <g className="buttons">
+          <circle cx="232" cy="56" r="4" fill="black"></circle>
+          <circle cx="232" cy="72" r="4" fill="black"></circle>
+          <circle cx="224" cy="64" r="4" fill="black"></circle>
+          <circle cx="240" cy="64" r="4" fill="black"></circle>
+        </g>
+      </svg>
+    );
 
     return (
       <GameboyScreen navigateBack={this.navigateBack} game={this.props.game} />
