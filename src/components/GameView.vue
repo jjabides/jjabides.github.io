e7b5371651add0911c8637092822ee2481e1f72b
DESKTOP-AFE8FR4\JJ <jj.abides@gmail.com>
2022-05-24 20:28:06 -0700

    Add back button

--- src/components/GameView.vue
+++ src/components/GameView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { computed, ref } from "vue";
+import resources from "./resources";
+import postal from "postal";
 
 var props = defineProps({
     image: String,
@@ -12,7 +14,12 @@ const fullscreen = ref(false);
 
 
 function back() {
+    var message = {
+        view: resources.views.cartridgesManager,
+    }
 
+    var channel = postal.channel("Notifications");
+    channel.publish("selectView", message);
 }
 
 function topLeftCornerClick() {
@@ -29,7 +36,10 @@ function topLeftCornerClick() {
     <div class="main-content">
         <div class="top-left-corner-space"></div>
         <div class="header">
-            <div class="title">{{ props.title }}</div>
+            <div class="row-1">
+                <div class="title">{{ props.title }}</div>
+                <div class="back-btn" @click="back">BACK</div>
+            </div>
         </div>
         <div class="description"> {{ props.description }}</div>
     </div>
@@ -86,12 +96,37 @@ function topLeftCornerClick() {
     display: inline-block;
     width: 60%;
     height: 30%;
-    justify-content: center;
 }
 
-.header .title {
+.row-1 {
+    display: flex;
+    padding: 16px;
+}
+
+.row-1 .title {
+    width: 100%;
     display: flex;
     justify-content: center;
+    align-items: center;
+}
+
+.row-1 .back-btn {
+    width: 100px;
+    height: 36px;
+    margin: 0px 0px 0px auto;
+    border-radius: 25px;
+    background-color: rgb(83, 83, 208);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    color: white;
+    transition: background-color .2s, color .2s;
+}
+
+.header .back-btn:hover {
+    background-color: white;
+    color: black;
 }
 
 .main-content .description {
